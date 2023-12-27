da3689e14fd6f23c1c9efa5173fd93d2294a39db
Aldana <aldananunez663@gmail.com>
2023-12-26 21:46:56 -0300

    Archivos subidos

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+});
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/contact.html'))
+});
+app.get('/music',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/music.html'))
+});
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/about.html'))
+});
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`)
+});
